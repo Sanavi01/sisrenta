d68d14bfdc5ff92e6b356281cca0329e1cd4288c
Santiago Avila <sanavidev@gmail.com>
2025-12-17 21:22:14 -0500

    First commit

---
+++ src/main/java/com/jirehcompanyit/sisrenta/SisrentaApplication.java
@@ -0,0 +1,13 @@
+package com.jirehcompanyit.sisrenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisrentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisrentaApplication.class, args);
+	}
+
+}
