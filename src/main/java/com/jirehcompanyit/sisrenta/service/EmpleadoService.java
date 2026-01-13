e7acd5b7e2099db3edf0d324f464f9b56c5a02ba
Santiago Avila <sanavidev@gmail.com>
2026-01-13 17:31:44 -0500

    Add: Documentacion del servicio de la entidad Empleado

--- src/main/java/com/jirehcompanyit/sisrenta/service/EmpleadoService.java
+++ src/main/java/com/jirehcompanyit/sisrenta/service/EmpleadoService.java
@@ -9,6 +9,19 @@ import com.jirehcompanyit.sisrenta.repository.EmpleadoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Servicio de la aplicacion responsable de los casos de uso relacionados con los empleados
+ *
+ * <p>Este servicio coordina la interaccion entre entidades de dominio, repositorios y
+ * validaciones necesarias para ejecutar operaciones sobre empleados, sin contener
+ * logica de negocio propia</p>
+ *
+ * <p>La logica de negocio y validaciones especificas del estado del cliente se delegan
+ * en la entidad {@link Empleado}</p>
+ *
+ * @author Santiago Angarita Avila
+ */
+
 @Service
 @Transactional
 public class EmpleadoService {
@@ -19,9 +32,24 @@ public class EmpleadoService {
         this.empleadoRepository = empleadoRepository;
     }
 
+    /**
+     * Registrar Empleado
+     *
+     * <p>Validaciones</p>
+     * <ul>
+     *     <li>Si no existe un empleado con un celular ya asociado a otro empleado</li>
+     * </ul>
+     *
+     * @param nombre    Nombres del empleado
+     * @param apellido  Apellidos del empleado
+     * @param celular   Celular del empleado
+     * @param direccion Direccion del empleado
+     * @return Cliente registrado
+     * @throws EmpleadoYaExisteException Si ya existe un empleado que tenga el celular a registrar asociado
+     */
+
     public Empleado registrarEmpleado(String nombre, String apellido, String celular, String direccion) {
 
-        System.out.println("Cliente llegado Service: " + nombre);
         empleadoRepository.findByCelular(celular)
                 .ifPresent(empleadoEncontrado -> {
                     throw new EmpleadoYaExisteException(
@@ -55,6 +83,24 @@ public class EmpleadoService {
                 .orElseThrow(() -> new EmpleadoNoEncontradoException("No hay ningun empleado registrado con numero de celular " + celular));
     }
 
+    /**
+     * Actualizar empleado
+     *
+     * <p>Permite actualizar la direccion y el numero de celular del cliente</p>
+     *
+     * <p>Validaciones</p>
+     *
+     * <ul>
+     *     <li>Que el celular a cambiar no pertenezca a otro empleado</li>
+     * </ul>
+     *
+     * @param id        Identificador del Empleado en BBDD
+     * @param celular   Mumero de celular del empleado
+     * @param direccion Direccion del empleado
+     * @return Empleado actualizado
+     * @throws EmpleadoYaExisteException Si el celular a cambiar ya esta asociado a otro cliente
+     */
+
     public Empleado actualizarEmpleado(Long id, String celular, String direccion) {
 
         Empleado empleadoActualizar = buscarEmpleadoPorId(id);
@@ -73,6 +119,18 @@ public class EmpleadoService {
         return empleadoRepository.save(empleadoActualizar);
     }
 
+    /**
+     * Activar Empleado
+     * <ul>
+     *     <li>Valida si el empleado existe a traves del id</li>
+     *     <li>Valida si el empleado ya esta activo</li>
+     * </ul>
+     *
+     * @param id Identificador del id
+     * @return Empleado activado
+     * @throws EmpleadoEstaActivoException Si el empleado esta activo
+     */
+
     public Empleado activarEmpleado(Long id) {
 
         Empleado empleadoEncontrado = buscarEmpleadoPorId(id);
@@ -90,6 +148,17 @@ public class EmpleadoService {
         return empleadoRepository.save(empleadoEncontrado);
     }
 
+    /**
+     * Desactivar Empleado
+     * <ul>
+     *     <li>Valida si el empleado existe a traves del id</li>
+     *     <li>Valida si el empleado ya esta desactivado</li>
+     * </ul>
+     *
+     * @param id Identificador del id
+     * @return Empleado desactivado
+     * @throws EmpleadoEstaActivoException Si el empleado esta inactivo
+     */
     public Empleado desactivarEmpleado(Long id) {
         Empleado empleadoEncontrado = buscarEmpleadoPorId(id);
         if (!empleadoEncontrado.isActivo()) {
